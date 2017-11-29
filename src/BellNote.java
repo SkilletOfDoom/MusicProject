62b26b88eb80a2994dba941635f00305234ef6d4
Andrew Kerwin <akerwin@carroll.edu>
2017-11-29 12:13:00 -0700

    Readded Functioning BellNote

---
+++ src/BellNote.java
@@ -0,0 +1,10 @@
+//class from Moodle coded by Nathan Williams
+public class BellNote {
+    final Note note;
+    final NoteLength length;
+
+    BellNote(Note note, NoteLength length) {
+        this.note = note;
+        this.length = length;
+    }
+}
